f91c951993945e6c1550b99d52e7bbe256597da4
Kunal Golani <kunal.golani@myntra.com>
2015-04-30 18:09:06 +0530

    test infra

---
+++ test/index.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var should = require('chai').should(),
+	Helenus = require('../lib');
+
+describe('dummy', function() {
+	it('should run', function() {
+		[].should.be.empty;
+	});
+});
